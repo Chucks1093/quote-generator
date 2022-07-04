bbdf918dcf95853ef73e4850f75f8b79f17b5c1c
Sebastian Anioke <aniokechukwudi7@gmail.com>
2022-07-04 20:52:01 +0100

    display text

--- script.js
+++ script.js
@@ -1,16 +1,48 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+
 let apiQuotes = [];
 
+// pick a random index
+function randomQuote(array) {
+    return Math.floor(Math.random() * array.length)
+}
+
+//Show new Quote
+function newQuote(array) {
+    const quote = array[randomQuote(array)]
+    console.log(quote)
+    // Check if author is null and changes it to unknown
+    if(!quote.author) {
+        authorText.textContent = "Unknown";
+    }else {
+        authorText.textContent = quote.author;
+    }
+    // Check quotes length to determine styling
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    }else{
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;
+}
 
 // Get quotes from API
-async function getQuotes() {
+async function fetchQuotes() {
     const apiUrl = 'https://type.fit/API/QUOTES';
     try{
         const response = await fetch(apiUrl);
         apiQuotes = await response.json();
-        console.log(apiQuotes[12])
-    }catch(error) {
-
+        newQuote(apiQuotes)
+    }catch (error) {
+        console.log(error)
     }
 }
-getQuotes()
-// console.log(3)
+
+fetchQuotes()
+// console.log(apiQuotes)
